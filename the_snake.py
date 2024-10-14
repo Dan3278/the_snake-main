aa4976b5f318eae702c4a71f39c9dcbdd62f819e
Данила Агапов <7904514482@mail.ru>
2024-10-14 19:30:10 +0300

    Corrected_Pep8_3

--- the_snake.py
+++ the_snake.py
@@ -1,30 +1,25 @@
 import pygame
 from random import randint
 
-# Константы для размеров поля и сетки
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 GRID_SIZE = 20
 GRID_WIDTH = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT = SCREEN_HEIGHT // GRID_SIZE
 
-# Направления движения
 UP = (0, -1)
 DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Цвета
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 BORDER_COLOR = (93, 216, 228)
 APPLE_COLOR = (255, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
 
-# Настройка игрового окна
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-pygame.display.set_caption("Змейка")
+pygame.display.set_caption('Змейка')
 
-# Настройка времени
 clock = pygame.time.Clock()
 
 
@@ -32,7 +27,7 @@ class GameObject:
 
     def __init__(self, position=(0, 0)):
         self.position = position
-        self.body_color = None  # Это будет переопределено в подклассах
+        self.body_color = None 
 
     def draw(self):
         raise NotImplementedError()
@@ -58,14 +53,12 @@ class Apple(GameObject):
 
 class Snake(GameObject):
 
-    def __init__(
-        self,
-        initial_position=(GRID_WIDTH // 2 * GRID_SIZE, GRID_HEIGHT // 2 * GRID_SIZE),
-    ):
+    def __init__(self, initial_position=(GRID_WIDTH // 2 * GRID_SIZE,
+                                          GRID_HEIGHT // 2 * GRID_SIZE)):
         super().__init__(initial_position)
         self.body = [self.position]
-        self.positions = self.body  # Новый атрибут positions
-        self.direction = (1, 0)  # Начальное направление вправо
+        self.positions = self.body
+        self.direction = (1, 0)
         self.body_color = SNAKE_COLOR
 
     def update(self):
@@ -75,10 +68,10 @@ class Snake(GameObject):
         )
         self.body.insert(0, new_position)
         self.position = new_position
-        self.positions = self.body  # Обновление атрибута positions
+        self.positions = self.body
 
         if len(self.body) > 1:
-            self.body.pop()  # Удаляем последний сегмент
+            self.body.pop()
 
     def draw(self):
         for position in self.body:
@@ -87,8 +80,8 @@ class Snake(GameObject):
             pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
     def grow(self):
-        self.body.append(self.body[-1])  # Увеличиваем длину змейки
-        self.positions = self.body  # Обновление атрибута positions
+        self.body.append(self.body[-1])
+        self.positions = self.body 
 
     def get_head_position(self):
         return self.body[0]
@@ -97,8 +90,8 @@ class Snake(GameObject):
         self.update()
 
     def reset(self):
-        self.body = [self.position]  # Сбрасываем тело
-        self.direction = (1, 0)  # Сбрасываем направление
+        self.body = [self.position]
+        self.direction = (1, 0)
 
     def update_direction(self, new_direction):
         if (new_direction[0] * -1, new_direction[1] * -1) != self.direction:
@@ -130,17 +123,16 @@ def main():
 
         snake.move()
 
-        # Обработка столкновений с яблоком
         if snake.get_head_position() == apple.position:
             snake.grow()
-            apple = Apple()  # Генерация нового яблока
+            apple = Apple()
 
         screen.fill(BOARD_BACKGROUND_COLOR)
         apple.draw()
         snake.draw()
 
         pygame.display.update()
-        clock.tick(10)  # Установить скорость игры
+        clock.tick(10)
 
 
 if __name__ == "main":
